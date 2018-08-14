d4256507771eb88d7808c61391df8ebd4e4c9aaa
Akanksha Rajwar <rajwar67@gmail.com>
2018-08-15 01:40:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/reddit_clone/Class_and_Adapter/InfoCard.java
@@ -0,0 +1,39 @@
+package com.example.android.reddit_clone.Class_and_Adapter;
+
+/**
+ * Created by Akanksha_Rajwar on 15-08-2018.
+ */
+
+public class InfoCard {
+    private String author;
+    private int imageres;
+    private int upvotes;
+    private int upcomments;
+    private int upshares;
+    private String title;
+
+
+    public InfoCard(String sentAuthor, String sentTitle, int sentImgRes, int sentUpvotes, int sentUpComments, int sentUpShares) {
+        author = sentAuthor;
+        imageres = sentImgRes;
+        title = sentTitle;
+        upvotes = sentUpvotes;
+        upcomments = sentUpComments;
+        upshares = sentUpShares;
+
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public int getImageres()
+    {
+        return imageres;
+    }
+}
+
